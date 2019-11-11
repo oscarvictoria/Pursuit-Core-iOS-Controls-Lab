0d1d3931d58ce0a477835ebde169135a66f0a046
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-11 15:59:28 -0500

    added outlets

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -1,7 +1,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    
+    @IBOutlet weak var stepperControl: UIStepper!
+    
+    
+    
+    @IBOutlet var cardValue: [UILabel]!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
